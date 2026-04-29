65453c7a0d53497b65d1c3cdc2815d622e63f293
Michael Graff <mgraff@cardinalhq.io>
2026-04-29 18:36:30 -0500

    add AGPL-3.0 SPDX license headers to source files (#8)

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -1,3 +1,6 @@
+<!-- SPDX-License-Identifier: AGPL-3.0-or-later -->
+<!-- Copyright (C) 2026 CardinalHQ, Inc. -->
+
 <script lang="ts">
   import { cartCount } from '../stores/cart';
   import Cart from './Cart.svelte';
